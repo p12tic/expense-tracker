43a4e9e23232c12d391a0cf40e5ff82346916864
Rosvaldas Atstupėnas <roskeats@gmail.com>
2024-08-23 20:16:50 +0300

    Added debug toolbar

--- expense_tracker/urls.py
+++ expense_tracker/urls.py
@@ -6,3 +6,9 @@ from django.conf.urls import url, include
 urlpatterns = [
     url(r'', include('expenses.urls')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        re_path(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
