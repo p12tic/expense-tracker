d3fc16977f2a990ec1ece6b540d99f6ce426896d
Rosvaldas Atstupėnas <roskeats@gmail.com>
2024-08-28 19:29:47 +0300

    Added Account interface

--- expense_tracker_frontend/src/components/Accounts.tsx
+++ expense_tracker_frontend/src/components/Accounts.tsx
@@ -2,10 +2,16 @@ import axios from "axios";
 import React, {useState} from "react";
 import {Link} from "react-router-dom";
 
-
+interface Account {
+    id: number;
+    name: string;
+    desc: string;
+    user: string;
+}
 export function Accounts() {
+    const [state, setState] = useState<Account[]>([]);
 
-    const [state, setState] = useState([]);
+
     let data;
 
     axios.get("http://localhost:8000/api/accounts").then(res => {
@@ -16,18 +22,22 @@ export function Accounts() {
         <>
             <div>
                 <table cellSpacing={200}>
-                    <th>Name</th>
-                    <th>Description</th>
-                    <th>Balance</th>
-                    <th></th>
-                    {state.map((output, id) => (
-                        <tr key = {id}>
-                            <td><Link to={`/accounts/${output.id}`}>{output.name}</Link></td>
-                            <td>{output.desc}</td>
-                            <td>{output.user}</td>
-                            <td><Link to={`/accounts/${output.id}/sync`} role="button" className="btn btn-xs btn-default pull-right">Sync</Link></td>
+                    <tbody>
+                        <tr>
+                            <th>Name</th>
+                            <th>Description</th>
+                            <th>Balance</th>
+                            <th></th>
                         </tr>
-                    ))}
+                        {state.map((output, id) => (
+                            <tr key={id}>
+                                <td><Link to={`/accounts/${output.id}`}>{output.name}</Link></td>
+                                <td>{output.desc}</td>
+                                <td>{output.user}</td>
+                                <td><Link to={`/accounts/${output.id}/sync`} role="button" className="btn btn-xs btn-default pull-right">Sync</Link></td>
+                            </tr>
+                        ))}
+                    </tbody>
                 </table>
             </div>
         </>
