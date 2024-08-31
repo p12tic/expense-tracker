233f89965032b25b34429a199f74b8f8ec00f3d8
Rosvaldas Atstupėnas <roskeats@gmail.com>
2024-09-01 02:11:08 +0300

    Added centsToString tool

--- expense_tracker_frontend/src/components/Tools.tsx
+++ expense_tracker_frontend/src/components/Tools.tsx
@@ -7,3 +7,28 @@ export const formatDate = (date: Date): string => {
         const minutes = String(date.getMinutes()).padStart(2, '0');
         return `${year}-${month}-${day} ${hours}:${minutes}`;
 };
+
+export function centsToString(value) {
+    try{
+        let Value: number = value;
+        if (Value % 100 == 0) {
+            return (Value / 100).toString();
+        }
+
+        const negative = Value < 0;
+        if (negative) {
+            Value = -Value;
+        }
+
+        const cents = Value % 100;
+        Value = Math.floor(Value / 100);
+        if (negative)
+            return `-${Value}.${cents.toFixed(0).padStart(2,'0')}`;
+        else
+            return `${Value}.${cents.toFixed(0).padStart(2,'0')}`;
+    }
+    catch{
+        return '';
+    }
+    // don't use floating-point numbers here due to potential rounding
+}
