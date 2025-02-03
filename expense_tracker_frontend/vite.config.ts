dbbf3671fd21cbb1a2d386e3dc4acbd63cdd5774
Povilas Kanapickas <povilas@radix.lt>
2025-02-03 02:40:44 +0200

    frontend: Setup vite dev proxy to upstream django server

--- expense_tracker_frontend/vite.config.ts
+++ expense_tracker_frontend/vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// django manage.py starts server on port 8000 by default
+const proxy = new URL('http://localhost:8000');
+
+const proxyTargetHttp = proxy.href.replace(/\/*$/, ''); // trim trailing slashes
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: proxyTargetHttp,
+        headers: {
+          'Host': proxy.host,
+          'Origin': proxyTargetHttp,
+        },
+        // note that changeOrigin does not work for POST requests
+      },
+    },
+  },
 })
