3ebb6e274f1ba50639c6b14703b3c47a8a9dd9c4
Rosvaldas Atstupenas <roskeats@gmail.com>
2025-03-21 07:55:56 +1100

    Install vite-plugin-checker

--- expense_tracker_frontend/vite.config.ts
+++ expense_tracker_frontend/vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import checker from 'vite-plugin-checker'
 // django manage.py starts server on port 8000 by default
 const proxy = new URL('http://localhost:8000');
 
 const proxyTargetHttp = proxy.href.replace(/\/*$/, ''); // trim trailing slashes
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), checker({typescript: true})],
   server: {
     proxy: {
       '/api': {
