a406fae4ff57652ba464be73bcb29e5b68ed4edb
Ahrirg <ldargevicius20@gmail.com>
2026-01-24 02:55:09 +0200

    Syntetic data generation

--- expenses/management/commands/syntetic_data_generator.py
+++ expenses/management/commands/syntetic_data_generator.py
@@ -1,12 +1,17 @@
 import random
 import string
+import datetime
 
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 from expenses.models import Tag
 from expenses.models import Account
+from expenses.models import Transaction
+from expenses.models import Subtransaction
+from expenses.models import AccountSyncEvent
 
 
 class Command(BaseCommand):
@@ -30,6 +35,19 @@ class Command(BaseCommand):
         user = self.get_user(
             username=options['username'],
             password=options['password'],
+        )
+
+        self.generate_data(
+            user=user,
+            amount_of_data_points = options['dataPoints'],
+            account_list = self.generate_accounts(
+                amount_of_accounts=options['accounts'],
+                user=user,
+            ),
+            tag_list = self.generate_tags(
+                amount_of_tags=options['tags'],
+                user=user
+            )
         )
 
     # ------------------------------------------------------------------------------
@@ -92,3 +110,27 @@ class Command(BaseCommand):
                 self.stdout.write(self.style.SUCCESS(f"New tag has been created with name: {current_tag.name}"))
         
         return tag_list
+
+    def generate_data(self, user: User, amount_of_data_points: int, account_list: list[Account], tag_list: list[Tag]) -> None:
+        if self.verbose:
+            self.stdout.write(f"Generating data for user: {user.username}")
+
+        for i in range(amount_of_data_points):
+            account = random.choice(account_list)
+            tag = random.choice(tag_list)
+            amount = self.random_amount()
+
+            Subtransaction.objects.create(
+                account=account,
+                transaction=Transaction.objects.create(
+                    user=user,
+                    desc=f"Transaction {self.random_string(5)}",
+                    date_time=datetime.datetime(random.randint(2000,2026), random.randint(1,12), random.randint(1,28), random.randint(0,12), 0, 0, tzinfo=timezone.UTC)
+                ),
+                amount=amount
+            )
+
+            if self.verbose:
+                self.stdout.write(f"Generated {i}/{amount_of_data_points} | Amount({amount}) has been added to account: {account.name}")
+            elif not self.verbose and i % 100 == 0:
+                self.stdout.write(f"Generated {i}/{amount_of_data_points}")
