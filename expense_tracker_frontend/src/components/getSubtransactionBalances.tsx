3b48f4fbe2306d87159e930fce25f03c6ef909ec
Ahrirg <ldargevicius20@gmail.com>
2026-02-03 23:21:11 +0200

    components: Added getAccountBalance() to getSubtransactionBalances.tsx

--- expense_tracker_frontend/src/components/getSubtransactionBalances.tsx
+++ expense_tracker_frontend/src/components/getSubtransactionBalances.tsx
@@ -1,4 +1,7 @@
-import {Dayjs} from "dayjs";
+import dayjs, {Dayjs} from "dayjs";
+
+import {AuthAxios} from "../utils/Network";
+import {formatDate} from "./Tools";
 
 interface Subtransaction {
   id: number;
@@ -21,6 +24,10 @@ interface SyncEvent {
   subtransaction: string;
 }
 
+interface AuthToken {
+  getToken: () => string;
+}
+
 export function getSubtransactionBalances(
   subtransactionBalancesProps: Subtransaction[],
 ) {
@@ -32,4 +39,36 @@ export function getSubtransactionBalances(
     return sum;
   });
   return sums;
+}
+
+export async function getAccountBalance(accountid: number, auth: AuthToken) {
+  const balanceRes = await AuthAxios.get(
+    `account_balance_cache?account=${accountid}`,
+    auth.getToken(),
+  );
+
+  let sum: number;
+  let lastCacheDate: Dayjs;
+
+  if (balanceRes.data.length > 0) {
+    sum = balanceRes.data[balanceRes.data.length - 1].balance;
+    lastCacheDate = dayjs(balanceRes.data[balanceRes.data.length - 1].date);
+  } else {
+    sum = 0;
+    lastCacheDate = dayjs(0);
+  }
+
+  const subRes = await AuthAxios.get(
+    `subtransactions?account=${accountid}&date_gte=${formatDate(lastCacheDate)}`,
+    auth.getToken(),
+  );
+  const subs: Subtransaction[] = subRes.data;
+
+  await Promise.all(
+    subs.map(async (sub) => {
+      sum = sum + sub.amount;
+    }),
+  );
+
+  return sum;
 }
